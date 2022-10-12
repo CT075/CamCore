dfb5827ac99053c34cb04029d5c6f7ca24f624d8
Cameron Wong <cwong@g.harvard.edu>
2022-10-12 16:38:31 -0400

    basic command line stuff
    Signed-off-by: Cameron Wong <cwong@g.harvard.edu>

--- bin/cli/src/main.rs
+++ bin/cli/src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "CamCore")]
+#[command(author = "CT075 <cam@camdar.io>")]
+#[command(version)]
+#[command(about = "An Event Assembler", long_about = None)]
+struct Opts {
+    game: String,
+    #[arg(long)]
+    debug: bool,
+    #[arg(short, long)]
+    verbose: bool,
+    #[arg(short, long)]
+    quiet: bool,
+    #[arg(long)]
+    build_times: bool,
+    #[arg(long)]
+    nocash_sym: bool,
+    #[arg(long)]
+    color: bool,
+}
+
 fn main() {
     println!("Hello, world!");
 }
